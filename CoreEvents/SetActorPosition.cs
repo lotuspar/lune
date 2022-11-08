1579b101b6aba77a26aeee109e610b4cc44b5dd6
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-09 02:37:43 +0800

    crevents: `SetActorPosition` & `SetActorVisibility`

---
+++ CoreEvents/SetActorPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Lune.CoreTypes;
+using Lune.Exceptions;
+
+namespace Lune.CoreEvents;
+
+public class SetActorPosition : Event
+{
+	public override string Name => "Set Actor Position";
+	[EventIO] public EventInput<Vector3> Position { get; set; }
+	[EventIO] public EventInput<Actor> Actor { get; set; }
+
+	public override void Start()
+	{
+		base.Start();
+
+		if ( Actor.Data == null )
+			throw new EventPropertyException( Actor );
+
+		Actor.Data.Position = Position;
+	}
+}
---
+++ CoreEvents/SetActorVisibility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Lune.CoreTypes;
+using Lune.Exceptions;
+
+namespace Lune.CoreEvents;
+
+public class SetActorVisibility : Event
+{
+	public override string Name => "Set Actor Visibility";
+	[EventIO] public EventInput<bool> IsVisible { get; set; }
+	[EventIO] public EventInput<Actor> Actor { get; set; }
+
+	public override void Start()
+	{
+		base.Start();
+
+		if ( Actor.Data == null )
+			throw new EventPropertyException( Actor );
+
+		Actor.Data.Model.RenderingEnabled = IsVisible?.Data ?? true;
+	}
+}
