d9d82d8c101b0331a1d18feb00041db417085833
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-09 02:36:27 +0800

    crtypes: basic scene actor work; `Actor` impl

---
+++ CoreTypes/Actor.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox;
+
+namespace Lune.CoreTypes;
+
+/// <summary>
+/// Actor for a Lune <see cref="Scene"/>
+/// </summary>
+public class Actor
+{
+	public Vector3 Position
+	{
+		get => Model.Position;
+		set => Model.Position = value;
+	}
+
+	public Rotation Rotation
+	{
+		get => Model.Rotation;
+		set => Model.Rotation = value;
+	}
+
+	public readonly SceneModel Model;
+
+	public Actor( SceneModel model )
+	{
+		Model = model;
+	}
+}
