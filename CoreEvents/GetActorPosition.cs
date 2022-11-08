0aa2ec7d7a780aa55560655a5c946ece6b24d5b1
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-09 02:42:58 +0800

    crevents: `GetActorPosition`

---
+++ CoreEvents/GetActorPosition.cs
@@ -0,0 +1,29 @@
+﻿using Lune.CoreTypes;
+using Lune.Exceptions;
+
+namespace Lune.CoreEvents;
+
+public class GetActorPosition : Event
+{
+	public override string Name => "Get Actor Position";
+	[EventIO] public EventInput<Actor> Actor { get; set; }
+	[EventIO] public EventOutput<Vector3> Position { get; set; }
+
+	public override void UpdateOutput( IEventOutput output )
+	{
+		base.UpdateOutput( output );
+
+		if ( output != Position )
+		{
+			return;
+		}
+
+		if ( Actor.Data == null )
+		{
+			throw new EventPropertyException( Actor );
+		}
+
+		Position.Data = new Vector3(
+			Actor.Data.Position.x, Actor.Data.Position.y, Actor.Data.Position.z );
+	}
+}
