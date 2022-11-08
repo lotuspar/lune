fa7f11fcde6fa2eb349cc82aa59c737f673f890c
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-09 02:30:28 +0800

    exceptions: `EventPropertyException` impl

---
+++ Exceptions/EventPropertyException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lune.Exceptions;
+
+/// <summary>
+/// Exception for <see cref="Event"/> IO property issues
+/// </summary>
+public class EventPropertyException : Exception
+{
+	public EventPropertyException()
+	{
+	}
+
+	public EventPropertyException( string property, string message = "Property not provided" )
+		: base( $"{property}: {message}" )
+	{
+	}
+
+	public EventPropertyException( IEventIO io, string message = "Property not provided" )
+		: base( $"{io.Name}: {message}" )
+	{
+	}
+
+	public EventPropertyException( string property, string message, Exception inner )
+		: base( $"{property}: {message}", inner )
+	{
+	}
+}
